0076c4f29c4fbc397cb7f0b6f31b28516405c300
Reinhold Kainhofer <r.kainhofer@wienerstaedtische.at>
2023-10-22 23:51:37 +0200

    Add warning message on package loading

--- R/HelperFunctions.R
+++ R/HelperFunctions.R
@@ -2,3 +2,7 @@
 #  Prevent spurious imports warnings in CRAN checks:
 #' @importFrom rmarkdown render
 NULL
+
+.onAttach <- function(libname, pkgname) {
+  warning("The LifeInsuranceContracts package has been renamed to LifeInsureR. \nPlease use library(LifeInsureR) instead!")
+}
